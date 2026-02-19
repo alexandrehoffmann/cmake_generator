50c6dd20fb009424e4c168c9bf36cf19264e42f7
Alexandre Hoffmann <alexandre.hoffmann.etu@gmail.com>
2026-02-19 14:19:26 +0100

    Update CMake minimum version and policy settings

--- cmake_generator.py
+++ cmake_generator.py
@@ -52,7 +52,9 @@ def create_folders(config):
 			os.makedirs(config.get("src_dir", default_src_dir) + "/" + library_data.get("folder", ""), exist_ok=True)
 
 def write_header(fout, config):
-	fout.write(u'cmake_minimum_required(VERSION 3.20)\n')
+	fout.write(u'cmake_minimum_required(VERSION 3.24)\n')
+	fout.write(u'\n')
+	fout.write(u'cmake_policy(SET CMP0135 NEW)\n')
 	fout.write(u'\n')
 	
 def write_project(fout, config):
@@ -70,7 +72,7 @@ def write_variables(fout, config):
 	write_section(fout, "Variables")
 	fout.write(u'\n')
 	fout.write(u'if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)\n')
-	fout.write(u'	set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Choose the type of build." FORCE)\n')
+	fout.write(u'	set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build.")\n')
 	fout.write(u'\n')
 	fout.write(u'	set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "RelWithDebInfo" "MinSizeRel")\n')
 	fout.write(u'endif()\n')
@@ -108,33 +110,29 @@ def write_options(fout, config):
 def write_dependencies(fout, config):
 	write_section(fout, "Dependencies")
 	fout.write(u'\n')
-	for dependency, data in config["dependencies"].items():
-		if data is None:
-			fout.write(u'find_package(' + dependency + u' REQUIRED)\n')
-		elif "version" in data:
-			if "git" in data:
-				fout.write(u'find_package(' + dependency + u' ' + data["version"] + u' QUIET CONFIG)\n')
-			else:
-				fout.write(u'find_package(' + dependency + u' ' + data["version"] + 'u REQUIRED)\n')
-		else:
-			if "git" in data:
-				fout.write(u'find_package(' + dependency + u' QUIET)\n')
-			else:
-				fout.write(u'find_package(' + dependency + u' REQUIRED)\n')
-	fout.write(u'\n')
 	fout.write(u'include(FetchContent)\n')
 	fout.write(u'\n')
 	for dependency, data in config["dependencies"].items():
+		if data is None:
+			fout.write(u'find_package(' + dependency + u' REQUIRED)\n')
+		elif "git" not in data and "version" in data:
+			fout.write(u'find_package(' + dependency + u' ' + data["version"] + 'u REQUIRED)\n')
+	fout.write(u'\n')
+	for dependency, data in config["dependencies"].items():
 		if data is not None and "git" in data:
-			fout.write(u'if (NOT ' + dependency + '_FOUND)\n')
-			fout.write(u'	message(STATUS "' + dependency + ' not found — falling back to FetchContent.")\n')
-			fout.write(u'	\n')
-			fout.write(u'	FetchContent_Declare(' + dependency + u' GIT_REPOSITORY ' + data["git"] + u' GIT_TAG ' + data.get("tag", default_git_tag) + u')\n')
-			fout.write(u'	FetchContent_MakeAvailable(' + dependency + ')\n')
-			fout.write(u'else()\n')
-			fout.write(u'	message(STATUS "Found ' + dependency + ' using system installation.")\n')
-			fout.write(u'endif()\n')
-			fout.write(u'\n')
+			dep_name = dependency
+			if "name" in data:
+				dep_name = data["name"]
+			
+			if "version" in data:
+				fout.write(u'FetchContent_Declare(' + dependency + u' GIT_REPOSITORY ' + data["git"] + u' GIT_TAG ' + data.get("tag", default_git_tag) + u' FIND_PACKAGE_ARGS NAMES ' + dep_name + ' ' + data["version"] + ')\n')
+			else:
+				fout.write(u'FetchContent_Declare(' + dependency + u' GIT_REPOSITORY ' + data["git"] + u' GIT_TAG ' + data.get("tag", default_git_tag) + u' FIND_PACKAGE_ARGS NAMES ' + dep_name + ')\n')
+	fout.write(u'\n')
+	for dependency, data in config["dependencies"].items():	
+		if data is not None and "git" in data:
+			fout.write(u'FetchContent_MakeAvailable(' + dependency + ')\n')
+	fout.write(u'\n')
 		
 def write_targets(fout, config):
 	write_section(fout, "Targets")
@@ -148,14 +146,9 @@ def write_targets(fout, config):
 		for library, data in config["libraries"].items():
 			if "sources" in data:
 				glob_obj_library.append(library)
-				fout.write(u'add_library(' + library + ' OBJECT)\n')
+				fout.write(u'add_library(' + library + ')\n')
 			else:
 				fout.write(u'add_library(' + library + u' INTERFACE)\n')
-	fout.write(u'\n')
-	fout.write(u'add_library(' + config["project_name"] + '::options ALIAS ' + config["project_name"] + '_options)\n')
-	fout.write(u'add_library(' + config["project_name"] + '::includes ALIAS ' + config["project_name"] + '_includes)\n')
-	for library in config.get("libraries", []):
-		fout.write(u'add_library(' + config["project_name"] + '::' + library + u' ALIAS ' + library + u')\n')
 	fout.write(u'\n')
 	if "executables" in config:
 		for executable, data in config["executables"].items():
@@ -178,20 +171,8 @@ def write_sources(fout, config):
 					fout.seek(fout.tell() - 1, os.SEEK_SET)
 					fout.write(u')\n')
 		fout.write(u'\n') 
-	for executable in config.get("executables", []):
-		if glob_obj_library:
-			fout.write(u'target_sources(' + executable + ' PRIVATE\n')
-			for obj_library in glob_obj_library:
-				fout.write(u'	$<TARGET_OBJECTS:' + obj_library +'>\n')
-	fout.seek(fout.tell() - 1, os.SEEK_SET)
-	fout.write(u')\n')
-	fout.write(u'\n')
 	
-def write_include_directories(fout, config):
-	target = config["project_name"] + '::includes'
-	
-	write_section(fout, "Includes")
-	fout.write(u'\n')
+	target = config["project_name"] + '_includes'
 	if "install" in config:
 		fout.write(u'target_include_directories(' + target + u' INTERFACE\n')
 		fout.write(u'	$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/' + config.get('include_dir', default_include_dir) + '>\n')
@@ -204,21 +185,21 @@ def write_compile_options(fout, config):
 	write_section(fout, "Compile options")
 	fout.write(u'\n')
 	for language, language_data in config.get("languages", default_languages).items():
-		fout.write(u'target_compile_features(' + config["project_name"] +  u'::options INTERFACE ' + language.lower() + '_std_' + str(language_data["standard"]) + ')\n')
+		fout.write(u'target_compile_features(' + config["project_name"] +  u'_options INTERFACE ' + language.lower() + '_std_' + str(language_data["standard"]) + ')\n')
 	fout.write(u'\n')
 	if glob_language_complier_options:
-		fout.write(u'target_compile_options(' + config["project_name"] + '::options INTERFACE\n')
+		fout.write(u'target_compile_options(' + config["project_name"] + '_options INTERFACE\n')
 		for compiler, language in glob_language_complier_options:
 			fout.write(u'	$<$<' + language + '_COMPILER_ID:' + get_compiler_id(compiler) + '>:${' + to_camel(config["project_name"]) + '_' + compiler.upper() + '_' + language + u'_COMPILE_OPTIONS}>\n')
 	fout.seek(fout.tell() - 1, os.SEEK_SET)
 	fout.write(u')\n')
 	fout.write(u'\n')
-	fout.write(u'if(MY_PROJECT_ENABLE_SANITIZERS)\n')
-	fout.write(u'	target_compile_options(' + config["project_name"] +  u'::options INTERFACE $<$<CONFIG:Debug>:${SAN_FLAGS})\n')
+	fout.write(u'if(' + to_camel(config["project_name"]) + u'_ENABLE_SANITIZERS)\n')
+	fout.write(u'	target_compile_options(' + config["project_name"] +  u'_options INTERFACE $<$<CONFIG:Debug>:${SAN_FLAGS}>)\n')
 	fout.write(u'endif()\n')
 	fout.write(u'\n')
-	fout.write(u'if(MY_PROJECT_USE_NATIVE_OPT)\n')
-	fout.write(u'	target_compile_options(' + config["project_name"] +  u'::options INTERFACE -march=native -mtune=native)\n')
+	fout.write(u'if(' + to_camel(config["project_name"]) + u'_USE_NATIVE_OPT)\n')
+	fout.write(u'	target_compile_options(' + config["project_name"] +  u'_options INTERFACE -march=native -mtune=native)\n')
 	fout.write(u'endif()\n')
 	fout.write(u'\n')
 	for targetType in ["libraries", "executables"]:
@@ -242,8 +223,8 @@ def write_link_libraries(fout, config):
 		for target, data in config["libraries"].items():
 			if target in glob_obj_library:
 				fout.write(u'target_link_libraries(' + target + '\n')
-				fout.write(u'	PRIVATE ' + config["project_name"] + '::options\n')
-				fout.write(u'	PUBLIC  ' + config["project_name"] + '::includes\n')
+				fout.write(u'	PRIVATE ' + config["project_name"] + '_options\n')
+				fout.write(u'	PUBLIC  ' + config["project_name"] + '_includes\n')
 				for dependency in data.get("public_dependencies", []):
 					fout.write('	PUBLIC ' + dependency + '\n')
 				for dependency in data.get("private_dependencies", []):
@@ -253,14 +234,14 @@ def write_link_libraries(fout, config):
 	if "executables" in config:
 		for target, data in config["executables"].items():
 			fout.write(u'target_link_libraries(' + target + u' PRIVATE\n')
-			fout.write(u'	' + config["project_name"] + u'::options\n')
-			fout.write(u'	' + config["project_name"] + u'::includes\n')
+			fout.write(u'	' + config["project_name"] + u'_options\n')
+			fout.write(u'	' + config["project_name"] + u'_includes\n')
 			for dependency in data.get("dependencies", []):
 				fout.write(u'	' + dependency + '\n')
 			fout.seek(fout.tell() - 1, os.SEEK_SET)
 			fout.write(u')\n\n')
-	fout.write(u'if(MY_PROJECT_ENABLE_SANITIZERS)\n')
-	fout.write(u'	target_link_options(' + config["project_name"] +  u'::options INTERFACE $<$<CONFIG:Debug>:${SAN_FLAGS})\n')
+	fout.write(u'if(' + to_camel(config["project_name"]) + u'_ENABLE_SANITIZERS)\n')
+	fout.write(u'	target_link_options(' + config["project_name"] +  u'_options INTERFACE $<$<CONFIG:Debug>:${SAN_FLAGS}>)\n')
 	fout.write(u'endif()\n\n')
 
 def write_tests(fout, config):
@@ -284,7 +265,6 @@ if __name__ == "__main__":
 		write_dependencies(fout, config)
 		write_targets(fout, config)
 		write_sources(fout, config)
-		write_include_directories(fout, config)
 		write_link_libraries(fout, config)
 		write_compile_options(fout, config)
 		write_tests(fout, config)
